7d8c7dab35e3064049e0a37324c0195f1306a3e5
Quesnel maxime <maxime.quesnel.contact@gmail.com>
2021-10-29 13:46:20 +0200

    WIP

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,4 @@
 import 'reflect-metadata';
-import { createConnection, getConnectionOptions } from 'typeorm';
-import { buildSchema } from 'type-graphql';
+import App from './server/apollo-server';
 
-import { ApolloServer } from 'apollo-server';
-
-import User from './resolvers/UserResolver';
-
-const app = async () => {
-  const connectionOptions = await getConnectionOptions();
-  await createConnection({
-    ...connectionOptions,
-    synchronize: true,
-    entities: ['dist/models/*.js']
-  });
-
-  const schema = await buildSchema({
-    resolvers: [User] // add resolvers in array
-  });
-
-  const resolvers = {};
-
-  const server = new ApolloServer({ schema, resolvers });
-
-  server.listen(8000, () => {
-    console.log(`🚀 Server ready at http://localhost:8000/graphql`);
-  });
-};
-
-app();
+const app = new App();
---
+++ src/server/apollo-server.ts
@@ -0,0 +1,34 @@
+import { ApolloServer } from 'apollo-server';
+import dotenv from 'dotenv';
+import { buildSchema } from 'type-graphql';
+import UserResolver from '../resolvers/UserResolver';
+
+dotenv.config();
+
+class App {
+  constructor() {
+    this.start();
+    this.connectToTheDatabase();
+  }
+
+  public async start() {
+    const schema = await buildSchema({
+      resolvers: [UserResolver],
+      emitSchemaFile: true
+    });
+    const server = new ApolloServer({ schema });
+    this.listen(server);
+  }
+
+  private async listen(server: ApolloServer) {
+    const port = process.env.PORT || '8001';
+    const { url } = await server.listen(port);
+    console.log(`App listening on the port ${port} at url ${url}/graphql`);
+  }
+
+  private connectToTheDatabase(): void {
+    console.log('connected');
+  }
+}
+
+export default App;
