fae339f89fcb869075ef93382ceb501be9085bf9
Quesnel maxime <maxime.quesnel.contact@gmail.com>
2021-12-02 21:21:55 +0100

    Add cypress and add first test for project

---
+++ cypress/integration/projects/create.spec.ts
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('test Project', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should click on a checkbox of an element', () => {
+    cy.get('#checkbox-selected-item-project').check();
+  });
+
+  it('should click on the input search and search element', () => {
+    cy.get('[data-testid="KeyboardArrowRightIcon"]').click();
+    cy.get('[data-testid="KeyboardArrowLeftIcon"]').click();
+    cy.get('.MuiOutlinedInput-input').type('Apple').blur();
+    cy.get('#enhanced-table-checkbox-0').contains('Apple');
+  });
+});
