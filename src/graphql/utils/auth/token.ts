8c559b058ec79a273f47107c8ee5bfa44fa03090
Quesnel maxime <maxime.quesnel.contact@gmail.com>
2022-01-10 22:17:25 +0100

    Initialize auth in back-end

---
+++ src/graphql/utils/auth/token.ts
@@ -0,0 +1,47 @@
+import { sign, verify, SignOptions } from 'jsonwebtoken';
+import { User } from '@prisma/client';
+
+export interface Token {
+  userId: User['id'];
+}
+
+const jwtOptions: Partial<SignOptions> = {
+  expiresIn: '1d',
+};
+
+const isValidToken = (token: Token): boolean => {
+  if (!token || !token.userId || typeof token.userId !== 'string') {
+    throw new Error('Invalid token');
+  }
+
+  return true;
+};
+
+export const signToken = (userId: Token['userId']): string => {
+  if (!process.env.APP_SECRET) {
+    throw new Error('Missing Secret');
+  }
+
+  const token = { userId };
+
+  if (isValidToken(token)) {
+    return sign({ userId }, process.env.APP_SECRET, jwtOptions);
+  }
+
+  throw new Error('Invalid Token');
+};
+
+export const verifyToken = (authorizationHeader: string): Token => {
+  if (!process.env.APP_SECRET) {
+    throw new Error('Missing Secret');
+  }
+
+  const token = authorizationHeader.replace('Bearer', '');
+  const verifiedToken = verify(token, process.env.APP_SECRET) as Token;
+
+  if (isValidToken(verifiedToken)) {
+    return verifiedToken;
+  }
+
+  throw new Error('Invalid token');
+};
