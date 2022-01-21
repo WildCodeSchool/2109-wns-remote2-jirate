7c48dfc52d205286b9370359e4dd697b5e2801fe
Quesnel maxime <maxime.quesnel.contact@gmail.com>
2022-01-21 09:20:06 +0100

    WIP

--- start.dev.sh
+++ start.dev.sh
@@ -1,5 +1,5 @@
 echo 'run build server docker image --------------'
- docker build -t server .
+docker build -t server .
 echo 'run docker compose -------------------------'
 yarn docker:up
 echo 'run migrations -----------------------------'
