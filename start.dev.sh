7e8eb1fc22fe93640953285a77955eb5af8df753
Maximeqnl <maxime.quesnel.contact@gmail.com>
2022-02-21 09:29:59 +0100

    WIP

--- start.dev.sh
+++
@@ -1,7 +0,0 @@
-echo 'run docker compose -------------------------'
-yarn docker:up
-
-# echo 'run migrations -----------------------------'
-# npx prisma migrate dev
-# echo 'run db:seed --------------------------------'
-# yarn db:seed
