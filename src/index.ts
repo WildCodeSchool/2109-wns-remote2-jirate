c0f4935703ebd6328c5c947adc1544a5c74aaa02
Maximeqnl <maxime.quesnel.contact@gmail.com>
2022-03-29 15:40:10 +0200

    Update login form

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,40 @@
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import dotenv from 'dotenv';
 import { performAstCodegen } from '@src/codegen';
 import schema from '@src/graphql/schema/schema';
+import corsOptions from './config/corsOptions';
 // import { IApolloServerContext } from '@src/lib/interfaces/IApolloServerContext';
 import prismaContext from '@src/lib/prisma/prismaContext';
-import authContext from '@src/lib/utils/authContext';
+import getUser from '@src/lib/utils/authContext';
 
 dotenv.config();
 
-const startServer = () => {
+const startServer = async () => {
   performAstCodegen();
 
   const apolloServerConfig = {
     schema,
     playground: process.env.NODE_ENV !== 'production',
-    context: req => {
+    context: ({ req, res }) => {
       prismaContext;
-      authContext(req);
+      const user = getUser(req);
+      console.log('AUTHORIZE USER', user);
+      return {
+        user,
+        req,
+        res,
+      };
     },
   };
 
   const server = new ApolloServer(apolloServerConfig);
+  await server.start();
+  const app: express.Application = express();
 
-  server
-    .listen({ port: 8000 })
-    .then(({ url }) => {
-      // eslint-disable-next-line no-console
-      console.log(`🚀  Server ready at ${url}graphql`);
-    })
-    // eslint-disable-next-line no-console
-    .catch(err => console.log('Error launching server', err));
+  server.applyMiddleware({ app });
+  app.listen({ port: 8000 }, () => {
+    console.log(`🚀 Server ready at http://localhost:${8000}${server.graphqlPath}`);
+  });
 };
-
 startServer();
--- src/lib/utils/authContext.ts
+++ src/lib/utils/authContext.ts
@@ -1,22 +1,16 @@
 import jwt from 'jsonwebtoken';
+import express from 'express';
 import { User } from '@prisma/client';
-import prismaContext from '@src/lib/prisma/prismaContext';
 
-const getUser = async (token: string): Promise<User | null> => {
-  if (!token) {
-    return null;
+const getUser = (req: express.Request): any => {
+  const token = req.headers.authorization || '';
+
+  if (token) {
+    const tokenValue = token.replace('Bearer ', '');
+    const user = jwt.verify(tokenValue, '121FfpfGJJU8Cff4GGSfVRT45CQZ3379D3D') as User;
+    return user;
   }
-
-  const userToken = jwt.verify(token, '121FfpfGJJU8Cff4GGSfVRT45CQZ3379D3D') as User;
-  return await prismaContext.prisma.user.findUnique({ where: { id: userToken.id } });
+  //return await prismaContext.prisma.user.findUnique({ where: { id: userToken.id } });
 };
 
-const authContext = async req => {
-  const header = req.req.headers.authorization;
-
-  const token = header.replace('Bearer ', '');
-
-  return { user: await getUser(token) };
-};
-
-export default authContext;
+export default getUser;
