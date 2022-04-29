a2ebb12f70014913ac11ebc6393a93401b28979b
selmaBBK <selma.boubakri@hotmail.com>
2022-04-29 14:39:30 +0200

    fix big

--- client/src/components/pages/Settings/Settings.jsx
+++ client/src/components/pages/Settings/Settings.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import SettingsNotifications from "./SettingsNotifications";
+import SettingsPassword from "./SettingsPassword";
 
 const Settings = props => {
     return (
         <div>
             <SettingsNotifications/>
-            <Settings/>
+            <SettingsPassword/>
         </div>
     );
 };
